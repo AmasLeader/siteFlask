009c584565611bace850138b8eb9f421cd3f0cfd
AmasLeader <remy.cocquempot@gmail.com>
2018-11-12 22:36:04 +0100

    Creation des templates et de la structure de base.html

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Rémy'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Accueil')
+    
+@app.route('/cv')
+def cv():
+    return render_template('cv.html', title='Curriculum')
+    
+@app.route('/hobby')
+def hobby():
+    return render_template('hobby.html', title='Passe-temps')
+    
+@app.route('/drawing')
+def drawing():
+    return render_template('drawing.html', title='Galerie')
